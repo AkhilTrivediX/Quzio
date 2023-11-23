c69c4718792b35d53105408e374c8385f9ae6eee
DevilsKingX <118957648+AkhilTrivediX@users.noreply.github.com>
2023-11-23 18:50:42 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: "default-src 'self'; connect-src 'self' vitals.vercel-insights.com",
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
